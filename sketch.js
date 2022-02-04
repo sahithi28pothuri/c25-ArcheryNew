503709a0ab6165ddc71b8129625bf84a2e5d388c
sahithi28pothuri <74135534+sahithi28pothuri@users.noreply.github.com>
2022-02-04 17:16:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,126 +4,113 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var canvas;
-var palyer, playerBase;
-var computer, computerBase;
-
-//Declare an array for arrows playerArrows = [ ]
-var playerArrows = [];
-
-var arrow;
-
-
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var polygon_img;
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-
+  createCanvas(900,400);
   engine = Engine.create();
   world = engine.world;
-
-  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.body.position.y - 180,
-    120,
-    120
-  );
-
-  computerBase = new ComputerBase(
-    width - 300,
-    random(450, height - 300),
-    180,
-    150
-  );
-  computer = new Computer(
-    width - 280,
-    computerBase.body.position.y - 153,
-    50,
-    180
-  );
-  computerArcher = new ComputerArcher(
-    width - 340,
-    computerBase.body.position.y - 180,
-    120,
-    120
-  );
-  
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
  
+  //level one
+  block1 = new Block(300,275,30,40);
+  console.log(block1);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  block16 = new Block(390,155,30,40);
 
+  blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40);
+  blocks3 = new Block(700,175,30,40);
+  blocks4 = new Block(730,175,30,40);
+  blocks5 = new Block(760,175,30,40);
+  blocks6 = new Block(670,135,30,40);
+  blocks7 = new Block(700,135,30,40);
+  blocks8 = new Block(730,135,30,40);
+  //top
+  blocks9 = new Block(700,95,30,40);
+
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:100,y:200});
 
 }
-
 function draw() {
-  background(180);
-
-  Engine.update(engine);
-
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
-
+  background(56,44,44); 
  
-  playerBase.display();
-  player.display();
-  
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
 
-  computerBase.display();
-  computer.display();
-  
-  playerArcher.display();
-  computerArcher.display()
+  ground.display();
+  stand1.display();
+  stand2.display();
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+  fill("skyblue");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("turquoise");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("pink")
+  blocks9.display();
+  fill("gold");
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
 
- // Use for loop to display arrow using showArrow() function
- for (var i = 0; i < playerArrows.length; i++) {
-  showArrows(i, playerArrows);
+  slingShot.display();
 }
 
-}
+//Create function for mouse dragged 
 
-function keyPressed() {
-
-  if(keyCode === 32){
-    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle+PI/2;
-
-    // var arrow = old PlayerArrow(posX, posY, 100, 10);
-    // var arrow = new PlayerArrow(posX, posY, 100, 10);
-    // var arrow = extend PlayerArrow(posX, posY, 100, 10);
-    // var arrow = New PlayerArrow(posX, posY, 100, 10);
-
-
-    // bow.trajectory = [];
-    // arrow.trajectory = [];
-    // bunny.trajectory = [];
-    // player.trajectory = [];
-
-    Matter.Body.setAngle(arrow.body, angle);
-    playerArrows.push(arrow);
-
-  }
-}
-
-function keyReleased () {
-
-  if(keyCode === 32){
-    //call shoot() function for each arrow in an array playerArrows
-    if (playerArrows.length) {
-      var angle = playerArcher.body.angle+PI/2;
-      playerArrows[playerArrows.length - 1].shoot(angle);
-    }
-  }
-
-}
-//Display arrow and Tranjectory
-function showArrows(index, arrows) {
-  arrows[index].display();
-  
-    
-  
- 
-
-}
+//Create function for mouse released
